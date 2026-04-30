d0e5f5ad66ffa4387e034b1298b681d27e6058f4
mateo-killer04 <mateotr711@gmail.com>
2026-04-30 13:16:17 +0200

    67 nigg, made ticket shop more expensive

--- index.js
+++ index.js
@@ -28307,7 +28307,7 @@ function BeeSwarmSimulator(DATA){
                 name:'ticket',
                 slot:'item',
                 viewMatrix:[-11.75-4.5,35,88.25-4.5,MATH.HALF_PI+MATH.QUATER_PI,0.02],
-                cost:[(n,i=1)=>Math.min(5000*i,1000000)+' honey'],
+                cost:[(n,i=1)=>Math.min(5000*i,10000000)+' honey'],
                 desc:'Can be used to purchase special items and activate machines!'
             }],
             currentIndex:0,message:'Explore Ticket Shop',currentIncrement:0,increments:[1,10,10e1,50e1]
